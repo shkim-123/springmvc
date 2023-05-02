d178e2ea5ced2a821501e1d782e863dbb38696e1
SH Kim <82145134+shkim-123@users.noreply.github.com>
2023-05-02 23:29:14 +0900

    http 요청 파라미터 - @ModelAttribute

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -3,6 +3,7 @@ package hello.springmvc.basic.request;
 import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -116,13 +117,27 @@ public class RequestParamController {
         return "ok";
     }
 
+    /**
+     * @ModelAttribute 사용
+     * 참고: model.addAttribute(helloData) 코드도 함께 자동 적용됨, 뒤에 model을 설명할 때 자세히 설명
+     */
     @ResponseBody
     @RequestMapping("/model-attribute-v1")
-    public String modelAttrivuteV1(@RequestParam String username, @RequestParam int age) {
-        HelloData helloData = new HelloData();
-        helloData.setUsername(username);
-        helloData.setAge(age);
+    public String modelAttrivuteV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData={}", helloData);
 
+        return "ok";
+    }
+
+    /**
+     * @ModelAttribute 생략 가능
+     * String, int 같은 단순 타입 = @RequestParam
+     * argument resolver 로 지정해둔 타입 외 = @ModelAttribute
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttrivuteV2(HelloData helloData) {
         log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
         log.info("helloData={}", helloData);
 
