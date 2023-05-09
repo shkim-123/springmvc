9ba91342046c2eaf82a76333d0b7242cf2ab76ed
SH Kim <82145134+shkim-123@users.noreply.github.com>
2023-05-09 23:15:55 +0900

    정리

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -27,6 +27,9 @@ public class LogTestController {
         log.warn("warn log={}", name);
         log.error("error log={}", name);
 
+        //로그를 사용하지 않아도 a+b 계산 로직이 먼저 실행됨, 이런 방식으로 사용하면 X
+        log.debug("String concat log=" + name);
+
         return "ok";
     }
 }
